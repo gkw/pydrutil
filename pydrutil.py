31e4394f505e10282d126260a634f410772a66e1
gkw <genkikuroda@gmail.com>
2015-05-18 23:15:16 -0700

    Create pydrutil.py

---
+++ pydrutil.py
@@ -0,0 +1,20 @@
+import subprocess
+
+def run(command, param):
+
+  outputj = {}
+  output = []
+  p = subprocess.Popen([command, param], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  for line in p.stdout.readlines():
+    output.append(line.replace('\n','').strip())
+
+  retval = p.wait()
+  outputj = {'command': command, 'param':param, 'output': output, 'returnVal': retval}
+  return outputj
+
+#test
+
+params = ['atip', 'eject','burn']
+
+for p in params:
+  print run("drutil",p)
